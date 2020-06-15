d28e6ccf2677d36e9ba9843fe62542084ffbe211
kiptubei <kiptubei@gmail.com>
2020-06-15 12:28:00 +0300

    Second draft of bubble sort implementation

--- Bubble-sort.rb
+++ Bubble-sort.rb
@@ -1,19 +1,26 @@
+# (2,1)  loop 1 check 0 < 1 swap  (1,2)
+# (2,3,1) pass 1[loop 1 check 0<1 swap  loop 2 check 1<2 swap] // pass2 [ loop 1 check 0<1 swap  loop 2 check 1<2 swap]
+# (2,3,5,6) pass 1[loop 1 check 0<1 swap  loop 2 check 1<2 swap loop3 check (3,4) swap ]// pass2 [ loop 1 check 0<1 swap  loop 2 check 1<2 swap] loop3 check (3,4) swap] // pass 3 [ loop 1 check 0<1 swap  loop 2 check 1<2 swap loop3 check (3,4) swap ]
+
+# bubble sort O(n^2)   n=2 pass =1 swap=1 n=3 pass=2 swaps=2 .....n=n
+
 def bubble_sort(array)
-    new_array=[]
-    x=0
-    while x<7
-        x +=1
-        for i in 0..(array.length-1)
-            if array[i].to_i < array[i+1].to_i
-                new_array.push(array[i])
-            else
-                new_array.push(array[i+1]) 
-            end 
-            
-        end
-        print new_array
+  array_size = array.length
+  print array
+  puts ''
+
+  (array_size - 1).times do |_pass|
+    (array_size - 1).times do |swap|
+      next unless array[swap] > array[swap + 1]
+
+      temp = array[swap + 1]
+      array[swap + 1] = array[swap]
+      array[swap] = temp
     end
-   
+  end
+  print array
 end
 
-bubble_sort([1,2,4,6,3,5])
+bubble_sort([4, 6, 3, 6, 7, 8, 0])
+
+# bubble_sort([7])
