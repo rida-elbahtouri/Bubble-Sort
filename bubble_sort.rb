f1c2378ce1fa4eba1befd3a0cad4e17aabb536cf
kiptubei <kiptubei@gmail.com>
2020-06-16 08:52:26 +0300

    Removed print and testing code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,7 +1,5 @@
 def bubble_sort(array)
   array_size = array.length
-  print array
-  puts ''
   (array_size - 1).times do |pass|
     (array_size - 1 - pass).times do |swap|
       next unless array[swap] > array[swap + 1]
@@ -13,10 +11,6 @@ def bubble_sort(array)
   end
   array
 end
-
-# testing bubble_sort
-
-print bubble_sort([4, 6, 3, 6, 7, 8, 0])
 
 def bubble_sort_by(arr_param)
   array_size = arr_param.length
@@ -31,14 +25,6 @@ def bubble_sort_by(arr_param)
       arr_param[swap + 1] = arr_param[swap]
       arr_param[swap] = temp
     end
-    print arr_param
-    puts ''
     return arr_param
   end
-end
-
-# testing bubble_sort_by
-
-bubble_sort_by(%w[hi hello hey]) do |left, right|
-  left.length - right.length
 end
